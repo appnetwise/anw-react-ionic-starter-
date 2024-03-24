27dd26558bf0cfbc107df836b02a63c257cf5288
Rashid Ali <rashid.ali@carbonplace.com>
2024-03-24 11:24:02 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'anw-react-ionic-starter',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
