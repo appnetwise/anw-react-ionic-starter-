bd888d2ea67882827674848b19c1028537eecb81
Rashid Ali <rashid.ali@carbonplace.com>
2024-03-24 11:34:04 +0530

    added button

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,12 +1,23 @@
-import './ExploreContainer.css';
+import React, { useState } from "react";
+import "./ExploreContainer.css";
 
-interface ContainerProps { }
+interface ContainerProps {}
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
+  const [message, setMessage] = useState("");
+
+  const handleClick = () => {
+    setMessage("Button clicked!");
+  };
+
   return (
     <div id="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <button onClick={handleClick} id="sayButton">
+        Say Hi
+      </button>
+      <div>
+        <span>{message}</span>
+      </div>
     </div>
   );
 };
